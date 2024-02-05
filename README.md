8aaa3e4178dbb6b609310c598ea59d2273fe5f65
Pushpender Singh <singhpushpender250@gmail.com>
2024-02-05 17:44:03 +0530

    updated

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LearnMate
 LearnMate is a AI powered Content Generation &amp; Adaptive Learning platform which Generates Tailored Video Content, Intelligent Quizzes, and Adaptive AI Tutorship - Guiding You Towards Mastery
+
+https://learnmate.onrender.com
+
+learnmate
+mT8fBEjktoPO6tfy
--- app.py
+++ app.py
@@ -1,22 +1,30 @@
-from flask import Flask, redirect, url_for, render_template, session
+from flask import Flask, redirect, url_for, render_template, session, request
 from flask_pymongo import PyMongo
 from flask_oauthlib.client import OAuth
+from functools import wraps
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGODB_URI          = os.environ.get('MONGODB_URI') 
+GOOGLE_CLIENT_ID     = os.environ.get('GOOGLE_CLIENT_ID')
+GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET') 
 
 app = Flask(__name__)
 
 # Configure MongoDB
-app.config['MONGO_URI'] = 'your_mongo_uri'
+app.config['MONGO_URI'] = MONGODB_URI
 mongo = PyMongo(app)
 
 # Configure Google OAuth
-app.secret_key = 'your_secret_key'
+app.secret_key = 'LearnMateProject#@6666'
 oauth = OAuth(app)
 google = oauth.remote_app(
     'google',
-    consumer_key='your_google_consumer_key',
-    consumer_secret='your_google_consumer_secret',
+    consumer_key=os.environ.get('GOOGLE_CLIENT_ID', GOOGLE_CLIENT_ID),
+    consumer_secret=os.environ.get('GOOGLE_CLIENT_SECRET', GOOGLE_CLIENT_SECRET),
     request_token_params={
-        'scope': 'email',
+        'scope': 'email profile',
     },
     base_url='https://www.googleapis.com/oauth2/v1/',
     request_token_url=None,
@@ -25,25 +33,45 @@ google = oauth.remote_app(
     authorize_url='https://accounts.google.com/o/oauth2/auth',
 )
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'google_token' not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route('/')
-def index():
+def login_page():
+    if 'google_token' in session:
+        return redirect('/dashboard')
+    else:
+        return render_template('index.html')
+
+@app.route('/login')
+def login():
     return google.authorize(callback=url_for('authorized', _external=True))
 
 @app.route('/logout')
 def logout():
     session.pop('google_token', None)
-    return redirect(url_for('index'))
+    return redirect('/')
+
+@google.tokengetter
+def get_google_oauth_token():
+    return session.get('google_token')
 
 @app.route('/login/authorized')
 def authorized():
-    resp = google.authorized_response()
-    if resp is None or resp.get('access_token') is None:
+    token = google.authorized_response()
+
+    if 'error' in token:
         return 'Access denied: reason={} error={}'.format(
             request.args['error_reason'],
             request.args['error_description']
         )
 
-    session['google_token'] = (resp['access_token'], '')
+    session['google_token'] = (token['access_token'], '')
     user_info = google.get('userinfo')
     user_id = user_info.data['id']
 
